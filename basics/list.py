9743d53321bfe843a995fe297ca4e8229940946c
thealphaversion <alphabuildv1@gmail.com>
2019-11-24 19:18:59 +0530

    lists updated

--- basics/list.py
+++ basics/list.py
@@ -41,3 +41,106 @@ long_list.append(1)                                                 # adds new v
 
 # here we add the value 55 after the element at the 0th index
 long_list.insert(0, 55)                                             # adds new value after the specified index
+
+
+
+
+# get the length of the list
+
+length_of_list = len(long_list)
+print("Length of List:", length_of_list)
+
+
+
+
+# delete elements from a list
+print("\nBefore removal:", long_list)
+long_list.remove(999)                                                # this will remove the element 55 from the list
+print("After removal:", long_list)
+
+# or top remove the last element use the method pop()
+long_list.pop()
+print("After popping:", long_list)
+
+# another way is to use the del keyword and the index of the element you want to delete
+del long_list[0]
+print("After deleting:", long_list)
+
+# to delete the entire list, just don't specify the index
+del long_list
+
+# or use the clear() method
+# long_list.clear()
+
+# print("List", long_list)                              # this line will give you an error
+                                                        # remove the comment to try
+
+
+
+# sometimes we want to make copies of lists
+# we do that using the method copy() and not the operator =
+# because the = operator only provides a reference to the orignal list
+
+orignal_list = [1, 5, 7, 15, 33, 46]
+
+copied_list = orignal_list.copy()
+
+reference_to_orignal_list = orignal_list
+
+# changes we do to the copied_list will not affect the orignal list
+# but changes to the reference_to_orignal_list will affect the orignal list
+
+print("\n-----------------------------------------------------------------------")
+
+print("Before any operation is done")
+print("Orignal List:", orignal_list)
+print("Copied List:", copied_list)
+print("Reference to the orignal list:", reference_to_orignal_list)
+
+# some insert, delete and manupulation operations
+
+copied_list[0] = 100
+
+reference_to_orignal_list[0] = 0
+
+print("\nAfter operations")
+print("Orignal List:", orignal_list)
+print("Copied List:", copied_list)
+print("Reference to the orignal list:", reference_to_orignal_list)
+
+print("-----------------------------------------------------------------------")
+
+
+# to combine two lists, use the + operator, or the extend() method
+# you can combine any type of data together
+
+first_list = [1, 3, 5, 7, 9]
+second_list = [2, 4, 6, 8]
+
+combined_list = first_list + second_list
+
+print("\nFirst list:", first_list)
+print("Second list:", second_list)
+print("Combined list:", combined_list)
+
+# the extend() method appends another list to one list
+
+print("\nFirst list:", first_list)
+print("Second list:", second_list)
+
+first_list.extend(second_list)
+
+print("\nFirst list:", first_list)
+
+# alternatively, you can loop through the lists and append elements indivisually to them
+
+list1 = ['a', 'b', 'c']
+list2 = [2, 5, 7]
+
+print("list 1:", list1)
+print("list 2:", list2)
+
+for element in list2:
+    list1.append(element)
+
+print("list 1 after appending:", list1)
---
+++ basics/sets.py
@@ -0,0 +1,9 @@
+# sets have elements that are unordered, unindexed and unique
+
+fruit_set = {"apple", "banana", "cherry"}
+print("Fruit Set:", fruit_set)
+
+# sets only contain unique elements
+
+random_set = {3, 4, 6, 6}
+print("\nRandom set:", random_set)                  # it will only print unique elements
