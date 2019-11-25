31333545a5c94e9d361e42ac308ce0cbc6a807a0
thealphaversion <alphabuildv1@gmail.com>
2019-11-26 00:43:44 +0530

    sets, tuples and stack added

--- basics/sets.py
+++ basics/sets.py
@@ -7,3 +7,40 @@ print("Fruit Set:", fruit_set)
 
 random_set = {3, 4, 6, 6}
 print("\nRandom set:", random_set)                  # it will only print unique elements
+
+# add elements to a set by using the add() method
+
+random_set.add(77)
+print("\nRandom set:", random_set)
+
+# to add multiple elements use the update() method
+random_set.update([23, 43, 66])
+
+# or
+
+a_list = [26, 34, 38, 83]
+random_set.update(a_list)
+ 
+print("\nRandom set after add and update:", random_set)
+
+# the update method updates the set by taking a union of the iterable with itself
+
+
+# remove elements from a set using the remove() or discard() methods
+
+random_set.remove(26)
+random_set.discard(83)
+
+print("\nRandom set after remove and discard:", random_set)
+
+# if the element to be removed does not exist then
+# remove() raises and error
+# discard() does not raise an error
+
+# delete the element using the clear() method or the del keyword
+
+random_set.clear()                                       # comment this line to try the del keyword below
+
+# or
+
+# del random_set                                         # remove the comment to try
---
+++ basics/tuples.py
@@ -0,0 +1,9 @@
+# tuples have elements that are ordered and cannot be changed
+
+fruits = ("apple", "banana", "orange", "mango")
+
+# access elements in a tuple using the indexes
+
+print("fruits[0]:", fruits[0])
+
+
