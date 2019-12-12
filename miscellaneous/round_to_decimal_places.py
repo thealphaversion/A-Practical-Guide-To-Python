2e5819c2fa1a33df1d88cf6ccfc66877eab23df7
thealphaversion <alphabuildv1@gmail.com>
2019-12-12 22:11:58 +0530

    updated string validators

--- miscellaneous/round_to_decimal_places.py
+++ miscellaneous/round_to_decimal_places.py
@@ -1,3 +1,5 @@
+# print any value with the required amount of precision
+
 float_value = 33.4531564
 
 print("%.2f" % round(float_value,2))
--- miscellaneous/string_validators.py
+++ miscellaneous/string_validators.py
@@ -1,26 +1,50 @@
+# check for different types of string validations
+
+# this function checks if the given string contains any alphanumeric characters or not
+def contains_alphanumeric(string):
+    if(any(character.isalnum() for character in string)):
+        return True
+    else:
+        return False
+
+
+# this function checks if the given string contains any alphabetical characters or not
+def contains_alphabets(string):
+    if(any(character.isalpha() for character in string)):
+        return True
+    else:
+        return False
+
+
+# this function checks if the given string contains any numbers or not
+def contains_digits(string):
+    if(any(character.isdigit() for character in string)):
+        return True
+    else:
+        return False
+
+# this function checks if the given string contains lowercase letters or not
+def contains_lowercase(string):
+    if(any(character.islower() for character in string)):
+        return True
+    else:
+        return False
+
+# this function checks if the given string contains uppercase letters or not
+def contains_uppercase(string):
+    if(any(character.isupper() for character in string)):
+        return True
+    else:
+        return False
+
+
 if __name__ == '__main__':
-    s = input()
-    if(any(string.isalnum() for string in s)):
-        print("True")
-    else:
-        print("False")
+    string_to_be_validated = "This Is A String 1234"
+    check = contains_lowercase(string_to_be_validated)
 
-    if(any(string.isalpha() for string in s)):
-        print("True")
+    # similarly you can try out the other validation functions
+    
+    if(check):
+        print("String is validated!")
     else:
-        print("False")
-
-    if(any(string.isdigit() for string in s)):
-        print("True")
-    else:
-        print("False")
-
-    if(any(string.islower() for string in s)):
-        print("True")
-    else:
-        print("False")
-
-    if(any(string.isupper() for string in s)):
-        print("True")
-    else:
-        print("False")
+        print("String is invalid!")
